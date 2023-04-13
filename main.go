8227eb07b3b44e39cf9cc4a8e7ae1a4584fe5eea
Lordkissa97 <christiaea@uia.no>
2023-04-13 19:58:44 +0200

    main go

--- main.go
+++ main.go
@@ -1,52 +1,53 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
 	"os"
-	"log"
-	"io"
-	"strings"
-	"github.com/uia-worker/misc/conv"
-)	
+	"strconv"
+)
 
 func main() {
-	src, err := os.Open("table.csv")
-	//src, err := os.Open("/home/janisg/minyr/kjevik-temp-celsius-20220318-20230318.csv")
+	// Åpner inputfilen og leser inn data
+	file, err := os.Open("kjevik-temp-celsius-20220318-20230318.csv")
 	if err != nil {
-        	log.Fatal(err)
+		panic(err)
 	}
-	defer src.Close()
-        log.Println(src)
-        
-	
-	var buffer []byte
-	var linebuf []byte // nil
-	buffer = make([]byte, 1)
-        bytesCount := 0
-	for {
-		_, err := src.Read(buffer)
-		if err != nil && err != io.EOF {
-			log.Fatal(err)
-		}
-
-		bytesCount++
-		//log.Printf("%c ", buffer[:n])
-		if buffer[0] == 0x0A {
-	           log.Println(string(linebuf))
-		   // Her
-		   elementArray := strings.Split(string(linebuf), ";")
-		   if len(elementArray) > 3 {
-			 celsius := elementArray[3]
-			 fahr := conv.CelsiusToFahrenheit(celsius)
-		         log.Println(elementArray[3])
-	   	   }
-                   linebuf = nil		   
-		} else {
-                   linebuf = append(linebuf, buffer[0])
-		}	
-		//log.Println(string(linebuf))
-		if err == io.EOF {
-			break
-		}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	data, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
 	}
 
+	// Konverterer temperaturene fra Celsius til Fahrenheit og lagrer resultatene i en ny liste
+	var convertedData [][]string
+	for i, row := range data {
+		if i == 0 { // overskriftsraden skal ikke endres
+			convertedData = append(convertedData, row)
+			continue
+		}
+		celsius, err := strconv.ParseFloat(row[1], 64)
+		if err != nil {
+			panic(err)
+		}
+		fahrenheit := (celsius * 9 / 5) + 32
+		convertedRow := []string{row[0], fmt.Sprintf("%.1f", fahrenheit)}
+		convertedData = append(convertedData, convertedRow)
+	}
+
+	// Skriver resultatene til outputfilen
+	outputFile, err := os.Create("kjevik-temp-fahr-20220318-20230318.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer outputFile.Close()
+	writer := csv.NewWriter(outputFile)
+	if err := writer.WriteAll(convertedData); err != nil {
+		panic(err)
+	}
+
+	// Skriver en avslutningsmelding til konsollen
+	fmt.Println("Konvertering fullført og lagret i kjevik-temp-fahr-20220318-20230318.csv")
+	fmt.Println("Data er basert på gyldig data (per 18.03.2023) (CC BY 4.0) fra Meteorologisk institutt (MET);endringen er gjort av STUDENTENS_NAVN")
 }
--- yr/yr.go
+++ yr/yr.go
@@ -1,11 +1,11 @@
 package yr
 
 import (
+	"errors"
 	"fmt"
-	"strconv"
-	//"strings"
-	//"errors"
 	"github.com/uia-worker/misc/conv"
+	"strconv"
+	"strings"
 )
 
 func CelsiusToFahrenheitString(celsius string) (string, error) {
@@ -18,15 +18,15 @@ func CelsiusToFahrenheitString(celsius string) (string, error) {
 	return fahrString, err
 }
 
-// Forutsetter at vi kjenner strukturen i filen og denne implementasjon 
+// Forutsetter at vi kjenner strukturen i filen og denne implementasjon
 // er kun for filer som inneholder linjer hvor det fjerde element
 // på linjen er verdien for temperaturaaling i grader celsius
 func CelsiusToFahrenheitLine(line string) (string, error) {
 
-        dividedString := strings.Split(line, ";")
+	dividedString := strings.Split(line, ";")
 	var err error
-	
-	if (len(dividedString) == 4) {
+
+	if len(dividedString) == 4 {
 		dividedString[3], err = CelsiusToFahrenheitString(dividedString[3])
 		if err != nil {
 			return "", err
@@ -35,8 +35,6 @@ func CelsiusToFahrenheitLine(line string) (string, error) {
 		return "", errors.New("linje har ikke forventet format")
 	}
 	return strings.Join(dividedString, ";"), nil
-	*/
-	
+
 	return "Kjevik;SN39040;18.03.2022 01:50;42.8", err
 }
-
